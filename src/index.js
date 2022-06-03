2edf8c008646c46fc08999cb863479eea9801714
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-03 02:15:01 -0700

    stub up the function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  // helper recursion for merging 2 sorted arrays
+  function helper(left, right, finalArr) {
+    // what to do if left and right arrays are not the same size
+  }
+}
