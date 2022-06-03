da5120d0e025ced13642cd1f1851fb8246680757
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-03 03:37:15 -0700

    correct to left and right arrays

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 function mergeSort(arr) {
   // helper recursion for merging 2 sorted arrays
-  function helper(left, right, finalArr) {
+  function helper(left, right, newArr) {
     let newArr = [];
     let i = 0;
     let j = 0;
     // while there are still values we haven't looked at
-    while (i < arr[i].length - 1 && j < arr[j].length - 1) {
+    while (i < left.length - 1 && j < right.length - 1) {
       // take a look at the smallest values in each input array
-      if (arr[i + 1] < arr[i]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
+      if (left[i + 1] < left[i]) {
+        let temp = left[i];
+        left[i] = left[i + 1];
+        left[i + 1] = temp;
         i++;
       } else {
         i++;
       }
-      if (arr[j + 1] < arr[j]) {
-        let temp1 = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp1;
+      if (right[j + 1] < right[j]) {
+        let temp1 = right[j];
+        right[j] = right[j + 1];
+        right[j + 1] = temp1;
         j++;
       } else {
         j++;
