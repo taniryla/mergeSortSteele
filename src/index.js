5cd01fa1b80b80f56a2a1a1810f4ef105545f9f3
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-03 03:07:01 -0700

    helper function logic

--- src/index.js
+++ src/index.js
@@ -4,8 +4,26 @@ function mergeSort(arr) {
     let newArr = [];
     let i = 0;
     let j = 0;
-    // take a look at the smallest values in each input array
-    while (i < j) {}
+
+    while (arr[i].length !== 0 && arr[j].length !== 0) {
+      // take a look at the smallest values in each input array
+      if (arr[i + 1] < arr[i]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        i++;
+      } else {
+        i++;
+      }
+      if (arr[j + 1] < arr[j]) {
+        let temp1 = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        j++;
+      } else {
+        j++;
+      }
+    }
     // what to do if left and right arrays are not the same size
   }
 }
