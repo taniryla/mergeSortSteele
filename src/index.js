34388898c6a1cbbd71d9017c5677914ee377d008
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-03 02:36:53 -0700

    counters in helper function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 function mergeSort(arr) {
   // helper recursion for merging 2 sorted arrays
   function helper(left, right, finalArr) {
+    let newArr = [];
+    let counterL = 0;
+    let counterR = 0;
+    // take a look at the smallest values in each input array
     // what to do if left and right arrays are not the same size
   }
 }
